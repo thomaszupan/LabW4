265432fa76ac44296556d26a8cbd09d4f0ea1cad
tazup <tazup@10.12.75.73>
2022-02-07 14:48:06 -0500

    init commit

--- src/lab_w4/Lab_W4.java
+++ src/lab_w4/Lab_W4.java
@@ -14,7 +14,10 @@ public class Lab_W4 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        
+        
+        
+        
     }
     
 }
