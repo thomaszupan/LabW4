d11472615ba4537d4ebbb2e3fcd03681b3e1b693
tazup <tazup@10.12.75.73>
2022-02-07 14:49:26 -0500

    new code lol

--- src/lab_w4/Lab_W4.java
+++ src/lab_w4/Lab_W4.java
@@ -14,7 +14,7 @@ public class Lab_W4 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        // new code master branchS
     }
     
 }
